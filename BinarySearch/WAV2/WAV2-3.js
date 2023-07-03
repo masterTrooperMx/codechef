d40c9bf91b8b42a9f5d219fa93b6ea17827e87a2
masterTrooperMx <mastertrooper@gmail.com>
2023-07-03 16:20:26 -0600

    better timming

---
+++ BinarySearch/WAV2/WAV2-3.js
@@ -0,0 +1,71 @@
+/**
+ * Chef is stuck in the wavey world of polynomials. 
+ * You are given all N roots of a polynomial P(x)=∏i=1N(x−ai). 
+ * The roots are pairwise distinct integers, but they are not given in any particular order.
+ * To help Chef escape, you should answer Q queries (numbered 1 through Q). 
+ * For each valid i, in the i-th query, you are given an integer xi​ and you have to determine whether P(xi) is positive, negative or 0.
+ * (x-a1)(x-a2)(x-a3)...(x-an) = 0
+ */
+console.time('WAV2-3');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', cacheInput).on('end', main);
+let input ='';
+
+/**
+ * The first line of the input contains two space-separated integers N and Q.
+ * The second line contains N space-separated integers a1,a2,…,aN​.
+ * Q lines follow. For each valid i, the i-th of these lines contains a single integer xi​ describing the i-th query.
+ */
+function cacheInput(data) {
+    input += data;
+}
+
+function prepareInput() {
+    input = input.split('\n'); //.map(Number);
+}
+
+const execQueries = (x, Poly) => {
+    let cero = false, pos = false;
+    let prod = 1;
+    for(let i = 0; i < Poly.length; i++){
+        prod *= x - Poly[i];
+        if(x - Poly[i] == 0){
+            cero = true;
+            console.log('0');
+            break;
+        }
+    }
+    if(cero == false){
+        (prod > 0)? pos = true : pos = false;
+        if(pos) {
+            console.log('POSITIVE');
+        } else {
+            console.log('NEGATIVE');
+        }
+    }
+    //return [cero, pos, prod];
+}
+
+function main() {
+    prepareInput();
+    let temp = input.splice(0, 1)[0]; // N, Q
+    const [N, Q] = temp.split(' ').map(Number);
+
+    temp = input.splice(0, 1)[0]; // Ais
+    let Ais = [];
+    if(typeof(temp) == "string") {
+        Ais = temp.split(" ").map(Number);
+    }
+
+    for(let i = 0; i < Q; i++) {
+        let x = Number(input.splice(0, 1)[0]); // xi
+        //console.log(execQueries(x, Ais));
+        execQueries(x, Ais)
+    }
+    //console.log(N, Q, Ais, xs);
+}
+
+main();
+console.timeEnd('WAV2-3');
