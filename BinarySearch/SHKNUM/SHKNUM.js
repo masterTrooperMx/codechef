a4810563a62783cc000884d4678935542e982c12
masterTrooperMx <mastertrooper@gmail.com>
2023-07-04 11:36:24 -0600

    para hacer suma de numeros triangular superior de matriz

---
+++ BinarySearch/SHKNUM/SHKNUM.js
@@ -0,0 +1,16 @@
+/**
+ * Sheokand is good at mathematics. One day, to test his math skills, Kaali gave him an integer N. 
+ * To impress Kaali, Sheokand has to convert N into an integer M that can be represented in the form 2^x+2^y 
+ * (where x and y are non-negative integers such that x<>y). 
+ * In order to do that, he can perform two types of operations:
+ *  add 1 to N
+ *  subtract 1 from N
+ * However, Sheokand is preparing for his exams. 
+ * Can you help him find the minimum number of operations required to convert N into a valid integer M?
+ */
+
+/**
+ * The first line of the input contains a single integer T denoting the number of test cases. 
+ * The description of T test cases follows.
+ * The first and only line of each testcase contains a single integer N.
+ */
